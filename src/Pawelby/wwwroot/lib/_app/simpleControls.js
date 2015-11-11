141458dbfcb9c1344a5f7016fd41598e1aafbf47
paweby <pawel.zhukov@gmail.com>
2015-11-11 11:18:41 +0300

    Revert "dev"
    This reverts commit c6746b12e8578ea78344da99b69541b010672ed7.

--- src/Pawelby/wwwroot/lib/_app/simpleControls.js
+++
@@ -1,1 +0,0 @@
-!function(){"use strict";function t(){return{scope:{show:"=displayWhen"},restrict:"E",templateUrl:"/views/wait-cursor.html"}}angular.module("simpleControls",[]).directive("waitCursor",t)}();
